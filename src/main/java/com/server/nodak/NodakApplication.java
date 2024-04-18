620c88ff86939bea5b93e0fab7c858c009971792
matt <matt1235@naver.com>
2024-04-18 23:24:32 +0900

    first commit

---
+++ src/main/java/com/server/nodak/NodakApplication.java
@@ -0,0 +1,13 @@
+package com.server.nodak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/nodak/NodakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.nodak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
