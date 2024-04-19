40edf85dda87b4317510fb160558ff7bbfc23abc
wooyong99 <wulovesk@naver.com>
2024-04-19 21:34:21 +0900

    test : VoteOption 테스트 코드 작성 (#2)

---
+++ src/test/java/com/server/nodak/domain/vote/utils/Utils.java
@@ -0,0 +1,24 @@
+package com.server.nodak.domain.vote.utils;
+
+import com.server.nodak.domain.vote.domain.Vote;
+import com.server.nodak.domain.vote.domain.VoteOption;
+import java.util.Random;
+
+public class Utils {
+    static Random rnd = new Random();
+
+    public static VoteOption createVoteOption(Vote vote, Integer seq, String content) {
+
+        return VoteOption.builder()
+                .vote(vote)
+                .content(content)
+                .seq((seq == null) ? rnd.nextInt(1, 10) : seq)
+                .build();
+    }
+
+    public static Vote createVote(String title) {
+        return Vote.builder()
+                .title(title)
+                .build();
+    }
+}
