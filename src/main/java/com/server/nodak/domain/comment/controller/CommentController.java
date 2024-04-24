3172c74b1fe811798baa35a79a548a10cde4e59c
matt <matt1235@naver.com>
2024-04-24 20:17:49 +0900

    feat: queryDsl 설정 파일, repository 생성 (#17)

--- src/main/java/com/server/nodak/domain/comment/controller/CommentController.java
+++ src/main/java/com/server/nodak/domain/comment/controller/CommentController.java
@@ -1,4 +1,7 @@
 package com.server.nodak.domain.comment.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
 }
--- src/main/java/com/server/nodak/domain/post/service/CategoryService.java
+++ src/main/java/com/server/nodak/domain/post/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.server.nodak.domain.post.service;
 
+import com.server.nodak.domain.post.repository.CategoryJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryJpaRepository categoryJpaRepository;
 }
