3366db49eb179753a6061b3a30a0739c135b5e12
matt <matt1235@naver.com>
2024-04-27 04:10:59 +0900

    feat: 댓글 생성 기능 구현 (#17)

--- src/main/java/com/server/nodak/domain/comment/controller/CommentController.java
+++ src/main/java/com/server/nodak/domain/comment/controller/CommentController.java
@@ -1,7 +1,24 @@
 package com.server.nodak.domain.comment.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.server.nodak.domain.comment.dto.request.CreateCommentRequest;
+import com.server.nodak.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/posts/{postId}/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(
+            @PathVariable("postId") long postId,
+            @RequestBody CreateCommentRequest commentRequest
+    ) {
+        commentService.createComment(postId, commentRequest);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/server/nodak/domain/comment/service/CommentService.java
+++ src/main/java/com/server/nodak/domain/comment/service/CommentService.java
@@ -1,4 +1,36 @@
 package com.server.nodak.domain.comment.service;
 
+import com.server.nodak.domain.comment.domain.Comment;
+import com.server.nodak.domain.comment.dto.request.CreateCommentRequest;
+import com.server.nodak.domain.comment.repository.CommentJpaRepository;
+import com.server.nodak.domain.comment.repository.CommentRepository;
+import com.server.nodak.domain.post.domain.Post;
+import com.server.nodak.domain.post.repository.PostJpaRepository;
+import com.server.nodak.domain.user.domain.User;
+import com.server.nodak.exception.common.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentJpaRepository commentJpaRepository;
+    private final PostJpaRepository postJpaRepository;
+
+    public void createComment(long postId, CreateCommentRequest commentRequest) {
+        Post post = postJpaRepository.findById(postId).orElseThrow(
+                () -> new DataNotFoundException());
+
+
+        // TODO : 로그인한 유저 넣기
+        Comment comment = Comment.builder()
+                .content(commentRequest.getContent())
+                .user(new User())
+                .post(post)
+                .build();
+
+        commentJpaRepository.save(comment);
+    }
 }
