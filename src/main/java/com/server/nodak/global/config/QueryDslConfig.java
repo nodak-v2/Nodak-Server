10456b41941283a149f9ffb50f482eace169c059
wooyong99 <wulovesk@naver.com>
2024-04-28 00:05:58 +0900

    feat : QueryDslConfig 추가

---
+++ src/main/java/com/server/nodak/global/config/QueryDslConfig.java
@@ -0,0 +1,15 @@
+package com.server.nodak.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory queryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
