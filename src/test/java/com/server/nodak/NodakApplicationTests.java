2961c9570281d88e4ceb13eed9fac75edb1fbc39
HongGeun <ghdrms163@gmail.com>
2024-04-28 16:45:46 +0900

    temp: 빌드 실패 테스트용

--- src/test/java/com/server/nodak/NodakApplicationTests.java
+++ src/test/java/com/server/nodak/NodakApplicationTests.java
@@ -1,13 +1,23 @@
 package com.server.nodak;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class NodakApplicationTests {
+	@Autowired
+	Environment env;
 
 	@Test
+	@DisplayName("빌드 실패용 테스트")
 	void contextLoads() {
+		String property = env.getProperty("spring.application.name");
+		System.out.println("========================");
+		System.out.println(property);
+		System.out.println("========================");
 	}
 
 }
