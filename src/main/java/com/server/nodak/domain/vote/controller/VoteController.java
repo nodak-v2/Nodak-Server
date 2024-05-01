0cd1e92b86025c009d048ace3cfc159b7632fe15
jungwooyong <85385921+wooyong99@users.noreply.github.com>
2024-05-01 21:57:44 +0900

    refactor : 테스트 코드 컨벤션 수정
    Co-authored-by: HaJungSu <56336436+Jungsu-lilly@users.noreply.github.com>

--- src/main/java/com/server/nodak/domain/vote/controller/VoteController.java
+++ src/main/java/com/server/nodak/domain/vote/controller/VoteController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/votes")
 public class VoteController {
+
     private final VoteService voteService;
 
     @GetMapping("/{voteId}")
