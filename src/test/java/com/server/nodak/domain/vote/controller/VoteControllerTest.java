818cf682fd407202ce6162054296154e438b31b8
wooyong99 <wulovesk@naver.com>
2024-05-01 21:18:44 +0900

    test : VoteController 테스트 코드 작성 #2

--- src/test/java/com/server/nodak/domain/vote/controller/VoteControllerTest.java
+++ src/test/java/com/server/nodak/domain/vote/controller/VoteControllerTest.java
@@ -1,11 +1,14 @@
 package com.server.nodak.domain.vote.controller;
 
 import static com.server.nodak.domain.vote.utils.Utils.createUser;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.server.nodak.domain.user.domain.User;
 import com.server.nodak.domain.vote.service.VoteService;
+import java.security.Principal;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,14 +19,18 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("VoteController 테스트")
 @Slf4j
 class VoteControllerTest {
+
     @InjectMocks
     VoteController voteController;
+    @Mock
+    Principal principal;
     @Mock
     VoteService voteService;
     MockMvc mockMvc;
@@ -36,14 +43,18 @@ class VoteControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(voteController)
                 .build();
     }
-     
+
     @Test
     @DisplayName("투표결과 조회 테스트")
     void voteResult() throws Exception {
         // Given
         Long voteId = 1L;
-        mockMvc.perform(get(String.format("/votes/%d", voteId)))
-                .andExpect(status().isOk());
+
+        // When
+        ResultActions resultActions = mockMvc.perform(get(String.format("/votes/%d", voteId)));
+
+        // Then
+        resultActions.andExpect(status().isOk());
     }
 
     @Test
@@ -51,11 +62,63 @@ class VoteControllerTest {
     void voteResultByVoteIdIsString() throws Exception {
         // Given
         String voteId = "voteId";
-        mockMvc.perform(get(String.format("/votes/%s", voteId)))
-                .andExpect(status().isBadRequest());
+
+        // When
+        ResultActions resultActions = mockMvc.perform(get(String.format("/votes/%s", voteId)));
+
+        // Then
+        resultActions.andExpect(status().isBadRequest());
     }
 
     @Test
-    void registerVote() {
+    @DisplayName("투표 등록 테스트")
+    void registerVote() throws Exception {
+        // Given
+        Long voteId = 1L;
+        Long optionId = 1L;
+        String username = "username";
+        given(principal.getName()).willReturn(username);
+
+        // When
+        ResultActions resultActions = mockMvc.perform(post(String.format("/votes/%d", voteId))
+                .param("option", String.valueOf(optionId))
+                .principal(principal));
+
+        // Then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("투표 등록 테스트 - voteId 값이 문자열이면 예외를 발생시킨다.")
+    void registerVoteByVoteIdIsString() throws Exception {
+        // Given
+        String voteId = "voteId";
+        Long optionId = 1L;
+        String username = "username";
+        given(principal.getName()).willReturn(username);
+
+        // When
+        ResultActions resultActions = mockMvc.perform(post(String.format("/votes/%s", voteId))
+                .param("option", String.valueOf(optionId))
+                .principal(principal));
+
+        // Then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("투표 등록 테스트 - optionId 값이 null이면 예외를 발생시킨다.")
+    void registerVoteByOptionIdIsNull() throws Exception {
+        // Given
+        Long voteId = 1L;
+        String username = "username";
+        given(principal.getName()).willReturn(username);
+
+        // When
+        ResultActions resultActions = mockMvc.perform(post(String.format("/votes/%d", voteId))
+                .principal(principal));
+
+        // Then
+        resultActions.andExpect(status().isBadRequest());
     }
 }
