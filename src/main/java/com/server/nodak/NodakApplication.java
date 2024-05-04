ec142018ea896c93ddf0b3162111afcb4875915b
HongGeun <ghdrms163@gmail.com>
2024-05-05 01:44:53 +0900

    Update NodakApplication.java

--- src/main/java/com/server/nodak/NodakApplication.java
+++ src/main/java/com/server/nodak/NodakApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.server.nodak")
 public class NodakApplication {
 
     public static void main(String[] args) {
