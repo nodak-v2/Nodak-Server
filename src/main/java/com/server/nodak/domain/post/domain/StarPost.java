61764533a5d71eaec911d3c8dd2055a78d7a1b9d
wooyong99 <wulovesk@naver.com>
2024-05-05 02:39:54 +0900

    feat : StarPostRepository 구현  #32

--- src/main/java/com/server/nodak/domain/post/domain/StarPost.java
+++ src/main/java/com/server/nodak/domain/post/domain/StarPost.java
@@ -2,18 +2,24 @@ package com.server.nodak.domain.post.domain;
 
 import com.server.nodak.domain.model.BaseEntity;
 import com.server.nodak.domain.user.domain.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "star_post")
-@SQLDelete(sql = "UPDATE star_post SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
 public class StarPost extends BaseEntity {
 
@@ -28,4 +34,26 @@ public class StarPost extends BaseEntity {
     @Column(name = "is_deleted", columnDefinition = "TINYINT(1)")
     @ColumnDefault("false")
     private boolean isDeleted;
+
+    @Builder
+    public StarPost(User user, Post post) {
+        setUser(user);
+        setPost(post);
+    }
+
+    private void setPost(Post post) {
+        if (this.post != null) {
+            this.post.getStarPosts().remove(this);
+        }
+        this.post = post;
+        this.post.getStarPosts().add(this);
+    }
+
+    private void setUser(User user) {
+        this.user = user;
+    }
+
+    public void delete(boolean isDeleted) {
+        this.isDeleted = isDeleted ? true : false;
+    }
 }
---
+++ src/main/java/com/server/nodak/domain/post/repository/StarPostRepository.java
@@ -0,0 +1,16 @@
+package com.server.nodak.domain.post.repository;
+
+import com.server.nodak.domain.post.domain.StarPost;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface StarPostRepository extends JpaRepository<StarPost, Long> {
+
+    boolean existsByUserIdAndPostId(Long userId, Long postId);
+
+    Optional<StarPost> findByUserIdAndPostId(Long userId, Long postId);
+
+    @Query("SELECT s FROM StarPost s WHERE s.user.id = :userId AND s.post.id = :postId")
+    Optional<StarPost> findByDeletedIsTrue(Long userId, Long postId);
+}
