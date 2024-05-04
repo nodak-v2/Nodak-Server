0ed33503ebdd4a526c228f0173bd13b4a3b4c60a
wooyong99 <wulovesk@naver.com>
2024-05-05 01:09:47 +0900

    fix : SwaggerConfig EnableWebMVC 제거  #32

--- src/main/java/com/server/nodak/global/config/SwaggerConfig.java
+++ src/main/java/com/server/nodak/global/config/SwaggerConfig.java
@@ -1,32 +1,22 @@
 package com.server.nodak.global.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
-import org.springdoc.core.configuration.SpringDocConfiguration;
-import org.springdoc.core.properties.SpringDocConfigProperties;
-import org.springdoc.core.properties.SwaggerUiConfigProperties;
-import org.springdoc.core.properties.SwaggerUiOAuthProperties;
-import org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@OpenAPIDefinition(info = @Info(title = "Nodak API 명세서", description = "Nodak API 명세서", version = "v1"))
+//@OpenAPIDefinition(info = @Info(title = "Nodak API 명세서", description = "Nodak API 명세서", version = "v1"))
 //@Configuration
-@EnableWebMvc
-@Import({SpringDocConfiguration.class,
-        SpringDocWebMvcConfiguration.class,
-        org.springdoc.webmvc.ui.SwaggerConfig.class,
-        SwaggerUiConfigProperties.class,
-        SwaggerUiOAuthProperties.class,
-        JacksonAutoConfiguration.class,
-        SpringDocConfigProperties.class})
+//@EnableWebMvc
+//@Import({SpringDocConfiguration.class,
+//        SpringDocWebMvcConfiguration.class,
+//        org.springdoc.webmvc.ui.SwaggerConfig.class,
+//        SwaggerUiConfigProperties.class,
+//        SwaggerUiOAuthProperties.class,
+//        JacksonAutoConfiguration.class,
+//        SpringDocConfigProperties.class})
 public class SwaggerConfig {
 
     private final static String BEARER_TOKEN_PREFIX = "Bearer";
