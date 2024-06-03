e73d3ed19d08ecd654fb2de5703a47d150dc0cdf
wooyong99 <wulovesk@naver.com>
2024-06-03 14:03:10 +0900

    refactor : Post 리스트 조회 응답 DTO 필드 수정 (#67)

--- src/main/java/com/server/nodak/domain/post/dto/PostSearchResponse.java
+++ src/main/java/com/server/nodak/domain/post/dto/PostSearchResponse.java
@@ -12,19 +12,22 @@ public class PostSearchResponse {
     private Long postId;
     private Long voteId;
     private String title;
-    private Long totalCount;
+    private Integer commentCount;
+    private Integer likeCount;
+    private Long voterCount;
     private String author;
     private String profileImageUrl;
     private String postImageUrl;
 
     @QueryProjection
-    public PostSearchResponse(Long postId, Long voteId, String title, Long totalCount, String author,
-                              String profileImageUrl,
-                              String postImageUrl) {
+    public PostSearchResponse(Long postId, Long voteId, String title, Integer commentCount, Integer likeCount,
+                              Long voterCount, String author, String profileImageUrl, String postImageUrl) {
         this.postId = postId;
         this.voteId = voteId;
         this.title = title;
-        this.totalCount = totalCount;
+        this.commentCount = commentCount;
+        this.likeCount = likeCount;
+        this.voterCount = voterCount;
         this.author = author;
         this.profileImageUrl = profileImageUrl;
         this.postImageUrl = postImageUrl;
