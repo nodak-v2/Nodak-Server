0187e010e36054f6ca21b4bd4dbabc553d11153b
HongGeun <ghdrms163@gmail.com>
2024-06-06 14:18:37 +0900

    fix : edit cors cookie policy

--- src/main/java/com/server/nodak/global/config/InterceptorConfig.java
+++ src/main/java/com/server/nodak/global/config/InterceptorConfig.java
@@ -3,6 +3,7 @@ package com.server.nodak.global.config;
 import com.server.nodak.security.aop.AuthorizationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,15 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Bean
     AuthorizationInterceptor authorizationInterceptor() {
         return new AuthorizationInterceptor();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie")
+                .allowCredentials(true);
     }
 }
