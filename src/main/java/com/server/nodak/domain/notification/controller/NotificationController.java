9371106e9e8447deb430615cde631064451c66c0
matt <matt1235@naver.com>
2024-06-11 21:10:57 +0900

    feat: user 유효성 검사 추가 및 리팩토링 (#75)

--- src/main/java/com/server/nodak/domain/notification/controller/NotificationController.java
+++ src/main/java/com/server/nodak/domain/notification/controller/NotificationController.java
@@ -4,11 +4,13 @@ import com.server.nodak.domain.follow.service.FollowService;
 import com.server.nodak.domain.post.domain.Post;
 import com.server.nodak.domain.user.domain.User;
 import com.server.nodak.domain.user.domain.UserRole;
+import com.server.nodak.exception.common.AuthorizationException;
 import com.server.nodak.security.aop.AuthorizationRequired;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -28,15 +30,29 @@ public class NotificationController {
 
     @GetMapping("/subscribe/{userId}")
     @AuthorizationRequired(UserRole.GENERAL)
-    public SseEmitter subscribe(Principal principal) {
-        long userId = Long.parseLong(principal.getName());
+    public SseEmitter subscribe(@PathVariable("userId") Long userId, Principal principal) {
+        validateUser(userId, principal);
 
-        SseEmitter emitter = new SseEmitter();
+        SseEmitter emitter = new SseEmitter(15 * 60 * 1000L);
         clients.put(userId, emitter);
+
         emitter.onCompletion(() -> clients.remove(userId));
         emitter.onTimeout(() -> clients.remove(userId));
 
+        try {
+            emitter.send(SseEmitter.event().name("init").data("Connected"));
+        } catch (IOException e) {
+            clients.remove(userId);
+        }
+
         return emitter;
+    }
+
+    private static void validateUser(Long userId, Principal principal) {
+        long currentUserId = Long.parseLong(principal.getName());
+        if (currentUserId != userId) {
+            throw new AuthorizationException();
+        }
     }
 
     public void notifyFollowers(User user, Post post) {
