a21f38b45d0ba1c19ea7e441d2eedfca8dde21c1
matt <matt1235@naver.com>
2024-06-12 06:58:24 +0900

    feat: Redis 관련 설정 추가 (#75)

---
+++ src/main/java/com/server/nodak/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.server.nodak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
