e04b23721ee1c560d3007a7a10a110b14e4ecb58
wooyong99 <wulovesk@naver.com>
2024-06-13 14:06:52 +0900

    refactor : UserController 사용기록 API 추가 (#88)

--- src/main/java/com/server/nodak/domain/user/controller/UserController.java
+++ src/main/java/com/server/nodak/domain/user/controller/UserController.java
@@ -1,18 +1,26 @@
 package com.server.nodak.domain.user.controller;
 
+import static com.server.nodak.global.common.response.ApiResponse.success;
+import static org.springframework.http.ResponseEntity.ok;
+
 import com.server.nodak.domain.user.domain.UserRole;
 import com.server.nodak.domain.user.dto.CurrentUserInfoResponse;
+import com.server.nodak.domain.user.dto.UserHistoryListResponse;
 import com.server.nodak.domain.user.dto.UserInfoResponse;
 import com.server.nodak.domain.user.dto.UserUpdateDTO;
 import com.server.nodak.domain.user.service.UserService;
 import com.server.nodak.global.common.response.ApiResponse;
 import com.server.nodak.security.aop.AuthorizationRequired;
+import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import static com.server.nodak.global.common.response.ApiResponse.*;
-import static org.springframework.http.ResponseEntity.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
@@ -37,4 +45,13 @@ public class UserController {
         userService.updateUserInfo(userUpdateDTO);
         return ok(success());
     }
+
+    @GetMapping("/history")
+    @AuthorizationRequired({UserRole.GENERAL, UserRole.MANAGER})
+    public ResponseEntity<ApiResponse<List<UserHistoryListResponse>>> getUserHistory(Principal principal) {
+        long userId = Long.parseLong(principal.getName());
+
+        return ResponseEntity.ok(success(userService.getUserHistory(userId)));
+    }
+
 }
