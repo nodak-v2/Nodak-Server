c7f7d9c4fa6ed0f8518bafd07e281be8be8b2a23
wooyong99 <wulovesk@naver.com>
2024-06-19 17:30:17 +0900

    refactor : PostRepository findByUserId 추가 (#99)

--- src/main/java/com/server/nodak/domain/post/repository/PostRepository.java
+++ src/main/java/com/server/nodak/domain/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.server.nodak.domain.post.domain.Post;
 import com.server.nodak.domain.post.dto.PostResponse;
 import com.server.nodak.domain.post.dto.PostSearchRequest;
 import com.server.nodak.domain.post.dto.PostSearchResponse;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +18,8 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostRepositor
     Optional<PostResponse> findOne(Long userId, Long postId);
 
     Optional<Post> findByIdAndUserId(Long postId, Long userId);
+
+    List<Post> findByUserId(Long userId);
 
     @Override
     Page<PostSearchResponse> findMyPosting(Long userId, Pageable pageable);
