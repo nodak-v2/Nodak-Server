e84d1f61ff26ae731ebf10ba1b8c104cff1aec86
matt <matt1235@naver.com>
2024-06-19 13:49:18 +0900

    feat: 게시글 응답에 종료 여부, 투표 선택지 텍스트 추가 (#98)

--- src/main/java/com/server/nodak/domain/post/dto/PostSearchResponse.java
+++ src/main/java/com/server/nodak/domain/post/dto/PostSearchResponse.java
@@ -3,6 +3,9 @@ package com.server.nodak.domain.post.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.annotation.Nullable;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,10 +26,15 @@ public class PostSearchResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime createdAt;
 
+    private boolean isTerminated;
+
+    @Nullable
+    private List<String> voteOptions;
+
     @QueryProjection
     public PostSearchResponse(Long postId, Long voteId, String title, Integer commentCount, Integer likeCount,
                               Long voterCount, String author, String profileImageUrl, String postImageUrl,
-                              LocalDateTime createdAt) {
+                              LocalDateTime createdAt, boolean isTerminated, List<String> voteOptions) {
         this.postId = postId;
         this.voteId = voteId;
         this.title = title;
@@ -37,5 +45,7 @@ public class PostSearchResponse {
         this.profileImageUrl = profileImageUrl;
         this.postImageUrl = postImageUrl;
         this.createdAt = createdAt;
+        this.isTerminated = isTerminated;
+        this.voteOptions = voteOptions;
     }
 }
