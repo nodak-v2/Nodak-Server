48a1c6c9930dc7fe697d5c41d36625ac86fbb011
wooyong99 <wulovesk@naver.com>
2024-06-27 22:20:19 +0900

    hotfix : PostSearchRequest @RequestBody 누락 코드 수정

--- src/main/java/com/server/nodak/domain/post/controller/PostController.java
+++ src/main/java/com/server/nodak/domain/post/controller/PostController.java
@@ -32,8 +32,9 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/search")
-    public ResponseEntity<ApiResponse<Page<PostSearchResponse>>> postSearch(PostSearchRequest request,
-                                                                            @PageableDefault Pageable pageable) {
+    public ResponseEntity<ApiResponse<Page<PostSearchResponse>>> postSearch(
+        @RequestBody PostSearchRequest request,
+        @PageableDefault Pageable pageable) {
         Page<PostSearchResponse> response = postService.findPostBySearch(request, pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
@@ -47,48 +48,54 @@ public class PostController {
     @GetMapping("/myPosting")
     @AuthorizationRequired(UserRole.GENERAL)
     public ResponseEntity<ApiResponse<Page<PostSearchResponse>>> myPosting(Principal principal,
-                                                                           @PageableDefault Pageable pageable) {
-        Page<PostSearchResponse> response = postService.findMyPosting(Long.parseLong(principal.getName()), pageable);
+        @PageableDefault Pageable pageable) {
+        Page<PostSearchResponse> response = postService.findMyPosting(
+            Long.parseLong(principal.getName()), pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     @GetMapping("/myVoteHistory")
     @AuthorizationRequired(UserRole.GENERAL)
     public ResponseEntity<ApiResponse<Page<PostSearchResponse>>> myVoteHistory(Principal principal,
-                                                                               @PageableDefault Pageable pageable) {
-        Page<PostSearchResponse> response = postService.findMyVoteHistory(Long.parseLong(principal.getName()),
-                pageable);
+        @PageableDefault Pageable pageable) {
+        Page<PostSearchResponse> response = postService.findMyVoteHistory(
+            Long.parseLong(principal.getName()),
+            pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     @GetMapping("/myComment")
     @AuthorizationRequired(UserRole.GENERAL)
     public ResponseEntity<ApiResponse<Page<PostSearchResponse>>> myComment(Principal principal,
-                                                                           @PageableDefault Pageable pageable) {
-        Page<PostSearchResponse> response = postService.findMyComment(Long.parseLong(principal.getName()),
-                pageable);
+        @PageableDefault Pageable pageable) {
+        Page<PostSearchResponse> response = postService.findMyComment(
+            Long.parseLong(principal.getName()),
+            pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     @GetMapping("/myLike")
     @AuthorizationRequired(UserRole.GENERAL)
     public ResponseEntity<ApiResponse<Page<PostSearchResponse>>> myLike(Principal principal,
-                                                                        @PageableDefault Pageable pageable) {
-        Page<PostSearchResponse> response = postService.findMyLike(Long.parseLong(principal.getName()),
-                pageable);
+        @PageableDefault Pageable pageable) {
+        Page<PostSearchResponse> response = postService.findMyLike(
+            Long.parseLong(principal.getName()),
+            pageable);
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     @PostMapping
     @AuthorizationRequired(UserRole.GENERAL)
-    public ResponseEntity<ApiResponse<Void>> registerPost(@RequestBody PostRequest request, Principal principal) {
+    public ResponseEntity<ApiResponse<Void>> registerPost(@RequestBody PostRequest request,
+        Principal principal) {
         postService.savePost(Long.parseLong(principal.getName()), request);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/{postId}/stars")
     @AuthorizationRequired(UserRole.GENERAL)
-    public ResponseEntity<ApiResponse<Void>> registerLike(@PathVariable Long postId, Principal principal) {
+    public ResponseEntity<ApiResponse<Void>> registerLike(@PathVariable Long postId,
+        Principal principal) {
         postService.registerLike(Long.parseLong(principal.getName()), postId);
         return ResponseEntity.ok().build();
     }
@@ -96,28 +103,31 @@ public class PostController {
     @PatchMapping("/{postId}")
     @AuthorizationRequired(UserRole.GENERAL)
     public ResponseEntity<ApiResponse<Void>> updatePost(@PathVariable Long postId,
-                                                        @RequestBody PostRequest request, Principal principal) {
+        @RequestBody PostRequest request, Principal principal) {
         postService.updatePost(Long.parseLong(principal.getName()), postId, request);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{postId}/stars")
     @AuthorizationRequired(UserRole.GENERAL)
-    public ResponseEntity<ApiResponse<Void>> cancleLike(@PathVariable Long postId, Principal principal) {
+    public ResponseEntity<ApiResponse<Void>> cancleLike(@PathVariable Long postId,
+        Principal principal) {
         postService.cancleLike(Long.parseLong(principal.getName()), postId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{postId}")
     @AuthorizationRequired(UserRole.GENERAL)
-    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable Long postId, Principal principal) {
+    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable Long postId,
+        Principal principal) {
         postService.removePost(Long.parseLong(principal.getName()), postId);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{postId}/terminate")
     @AuthorizationRequired(UserRole.GENERAL)
-    public ResponseEntity<ApiResponse<Void>> terminateVote(@PathVariable Long postId, Principal principal) {
+    public ResponseEntity<ApiResponse<Void>> terminateVote(@PathVariable Long postId,
+        Principal principal) {
         postService.terminateVote(Long.parseLong(principal.getName()), postId);
         return ResponseEntity.ok().build();
     }
