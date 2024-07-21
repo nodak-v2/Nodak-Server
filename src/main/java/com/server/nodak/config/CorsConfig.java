ccccbe6974c824a1d0e9238d481827c0eeb9a06d
wooyong99 <wulovesk@naver.com>
2024-07-21 15:03:59 +0900

    hotfix : localhost:3000 cors 허용

--- src/main/java/com/server/nodak/config/CorsConfig.java
+++ src/main/java/com/server/nodak/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
